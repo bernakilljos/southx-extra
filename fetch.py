4026fe5f2b8f6138e426fbc49f0b429f0b7b2ab1
x <x@y.zz>
2026-05-11 22:59:55 -0700

    ind

--- fetch.py
+++ fetch.py
@@ -1,14 +1,11 @@
 import requests,os
+os.makedirs('data/ind',exist_ok=True)
 base='https://moderngov.southwark.gov.uk/'
-s=requests.Session();
-r=s.get(base+'ieListDocuments.aspx?CId=519&MId=8249');print('cookie',s.cookies.get_dict())
+from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-u=urljoin(base,'documents/s131118/Local%20Practice%20Menopause%20in%20Southwark.pdf')
-for i,kwargs in enumerate([
- {}, {'headers':{'Referer':r.url}}, {'headers':{'Referer':r.url,'User-Agent':'Mozilla/5.0'}}, {'params':{'T':9}}, {'params':{'T':10}}, {'params':{'CT':2}}, {'params':{'T':0,'CT':2,'MID':8249}},
-]):
- r2=s.get(u,**kwargs); print(i,r2.url,r2.status_code,len(r2.content),r2.text[:80] if len(r2.content)<300 else '')
- os.makedirs('data',exist_ok=True);open(f'data/try{i}','wb').write(r2.content)
-for i,url in enumerate(['mgChooseDocPack.aspx?ID=8249','mgChooseDocPack.aspx?ID=8320']):
- rr=s.get(urljoin(base,url)); print('packpage',i,rr.status_code,len(rr.content),rr.url)
- open(f'data/choose{i}.html','wb').write(rr.content)
+headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64)'}
+s=requests.Session();s.headers.update(headers)
+for i,p in enumerate(['ieListDocuments.aspx?CId=519&MId=8249','ieListDocuments.aspx?CId=171&MId=8320']):
+ r=s.get(urljoin(base,p));soup=BeautifulSoup(r.text,'html.parser');open(f'data/new_m{i}.html','wb').write(r.content);print(i,'page',r.status_code,len(r.content))
+ for j,a in enumerate(soup.select('a[href*="documents/s"]')):
+  u=urljoin(r.url,a['href']);r2=s.get(u,headers={'Referer':r.url,'User-Agent':'Mozilla/5.0'}); print(i,j,r2.status_code,len(r2.content),a['href'][:60]);open(f'data/ind/m{i}_{j}.pdf','wb').write(r2.content)
