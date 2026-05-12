9c867f378ff30dde764d0172591f0c969252219f
x <x@y.zz>
2026-05-11 22:23:03 -0700

    links

--- fetch.py
+++ fetch.py
@@ -1,8 +1,20 @@
-import requests
+import requests,os
 base='https://moderngov.southwark.gov.uk/'
-paths=['mgCalendarMonthView.aspx?GL=1&bcr=1','mgCalendarMonthView.aspx?GL=1&bcr=1&M=1&Y=2026','mgCalendarMonthView.aspx?bcr=1&GL=1&NoBdr=1','mgCalendarMonthView.aspx?M=1&Y=2026&GL=1&bcr=1','mgCalendarMonthView.aspx?M=1&Y=2026&bcr=1']
-import os;os.makedirs('data',exist_ok=True)
-for i,p in enumerate(paths):
- r=requests.get(base+p);open(f'data/cal{i}.html','wb').write(r.content);print(i,r.status_code,len(r.content),r.url)
-for i,h in enumerate(['https://southwark.moderngov.co.uk/','http://moderngov.southwark.gov.uk/']):
- r=requests.get(h+'mgCalendarMonthView.aspx?GL=1&bcr=1&M=1&Y=2026');open(f'data/alt{i}.html','wb').write(r.content);print('alt',i,r.status_code,len(r.content),r.url)
+# fetch weekly meetings plus all linked docs recursively
+pages=['ieListDocuments.aspx?CId=172&MId=8402','ieListDocuments.aspx?CId=327&MId=8405','ieListDocuments.aspx?CId=519&MId=8249','ieListDocuments.aspx?CId=650&MId=8178','ieListDocuments.aspx?CId=172&MId=8403','ieListDocuments.aspx?CId=171&MId=8320']
+os.makedirs('data',exist_ok=True)
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin,urlparse
+for i,p in enumerate(pages):
+ r=requests.get(urljoin(base,p));print(i,p,r.status_code,len(r.content),r.url)
+ open(f'data/m{i}.html','wb').write(r.content)
+ s=BeautifulSoup(r.text,'html.parser')
+ # capture PDFs/doc links
+ j=0
+ for a in s.select('a[href]'):
+  href=a['href']; ful=urljoin(r.url,href)
+  if not any(x in href.lower() for x in ['.pdf','.doc','mgconvert2pdf','mgconvert2pdf','publicpack.pdf','publicpack agenda']):continue
+  try:r2=requests.get(ful,timeout=60);print('  link',j,href[:80],r2.status_code,len(r2.content),r2.url)
+  except Exception as e:print('  err',e);continue
+  ext='.pdf' if b'%PDF' in r2.content[:20] else '.html'
+  open(f'data/m{i}_{j}{ext}','wb').write(r2.content); j+=1
